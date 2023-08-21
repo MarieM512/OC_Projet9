526fe65002567591d832375792c2394255526058
Marie Metay <mariemetay.pro@gmail.com>
2023-08-21 18:09:35 +0200

    Add BottomNavigationBar

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/composant/bottomNavigation/BottomNavigation.kt
@@ -0,0 +1,90 @@
+package com.openclassrooms.realestatemanager.ui.composant.bottomNavigation
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.ui.AddView
+import com.openclassrooms.realestatemanager.ui.FilterView
+import com.openclassrooms.realestatemanager.ui.ListView
+import com.openclassrooms.realestatemanager.ui.LoanView
+import com.openclassrooms.realestatemanager.ui.MapView
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavItem.Map.route) {
+        composable(BottomNavItem.Map.route) {
+            MapView()
+        }
+        composable(BottomNavItem.List.route) {
+            ListView()
+        }
+        composable(BottomNavItem.Add.route) {
+            AddView()
+        }
+        composable(BottomNavItem.Filter.route) {
+            FilterView()
+        }
+        composable(BottomNavItem.Loan.route) {
+            LoanView()
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    navController: NavHostController,
+) {
+    val views = listOf(
+        BottomNavItem.Map,
+        BottomNavItem.List,
+        BottomNavItem.Add,
+        BottomNavItem.Filter,
+        BottomNavItem.Loan,
+    )
+    NavigationBar(
+        modifier = Modifier,
+        containerColor = colorResource(id = R.color.colorAccent),
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        views.forEach { view ->
+            NavigationBarItem(
+                label = {
+                    Text(text = view.title)
+                },
+                icon = {
+                    Icon(painterResource(id = view.icon), contentDescription = "")
+                },
+                selected = currentRoute == view.route,
+                onClick = {
+                    navController.navigate(view.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedTextColor = colorResource(id = R.color.colorPrimary),
+                    selectedIconColor = colorResource(id = R.color.colorPrimary),
+                    indicatorColor = colorResource(id = R.color.colorAccent),
+                ),
+            )
+        }
+    }
+}
