dedd7d64d95a705e316140eddf46a2cf9ba488d5
Marie Metay <mariemetay.pro@gmail.com>
2023-09-15 16:15:13 +0200

    Add Edit to ListScreen for tablet

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/detail/DetailTabletScreen.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/detail/DetailTabletScreen.kt
@@ -10,14 +10,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -68,14 +70,32 @@ fun DetailTabletScreen(
                                 .fillMaxWidth(),
                             horizontalAlignment = Alignment.CenterHorizontally,
                         ) {
-                            Text(
-                                text = property.status.label,
-                                color = if (property.status == Status.AVAILABLE) {
-                                    Color.Green
-                                } else {
-                                    MaterialTheme.colorScheme.error
-                                },
-                            )
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                Text(
+                                    text = property.status.label,
+                                    color = if (property.status == Status.AVAILABLE) {
+                                        Color.Green
+                                    } else {
+                                        MaterialTheme.colorScheme.error
+                                    },
+                                )
+                                IconButton(
+                                    onClick = {
+                                        navController.navigate("edit/$property")
+                                    },
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Edit,
+                                        contentDescription = "Edit property",
+                                        tint = Color.Black,
+                                    )
+                                }
+                            }
                         }
                         Row(
                             horizontalArrangement = Arrangement.SpaceBetween,
